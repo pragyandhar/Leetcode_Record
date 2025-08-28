b1f68a013451c247718b8b990771aece5e9bbf7c
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-08-28 07:35:47 +0530

    Time: 1 ms (84.24%), Space: 41.9 MB (23.54%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -2,7 +2,7 @@ class Solution {
     public String smallestNumber(String pattern) {
         int n = pattern.length();
         int count = 1;
-        String result = "";
+        StringBuilder result = new StringBuilder();
         Stack<String> num = new Stack<>();
 
         for (int i = 0; i < n+1; i++) {
@@ -11,8 +11,7 @@ class Solution {
 
             if (i == n || pattern.charAt(i) == 'I') {
                 while (!num.isEmpty()) {
-                    result += num.peek();
-                    num.pop();
+                    result.append(num.pop());
                 }
             }
         }
