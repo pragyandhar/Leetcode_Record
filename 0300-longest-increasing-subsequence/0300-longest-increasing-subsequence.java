55b25c217ab71ffac3e18f8f8f9ce73a8c88d3b5
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-09-17 12:07:11 +0530

    Time: 2 ms (99.54%), Space: 44.5 MB (50.5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,16 +1,49 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
+        /* TC = O(n) */
 
+        // int[] dp = new int[nums.length];
+        // Arrays.fill(dp, 1);
+
+        // for (int i = 1; i < dp.length; i++) {
+        //     for (int j = i-1; j >= 0; j--) {
+        //         if (nums[i] > nums[j]) {
+        //             int x = dp[j];
+        //             dp[i] = Math.max(dp[i], x+1);
+        //         }
+        //     }
+        // }
+        // return Arrays.stream(dp).max().getAsInt();
+        return LIS(nums);
+    }
+
+    public static int LIS(int[] arr) {
+        int[] dp = new int[arr.length];
+        int len = 1;
+        dp[0] = arr[0];
         for (int i = 1; i < dp.length; i++) {
-            for (int j = i-1; j >= 0; j--) {
-                if (nums[i] > nums[j]) {
-                    int x = dp[j];
-                    dp[i] = Math.max(dp[i], x+1);
-                }
+            if (arr[i] > dp[len-1]) {
+                dp[len] = arr[i];
+                len++;
+            } else {
+                int idx = BinarySearch(dp, 0, len-1, arr[i]);
+                dp[idx] = arr[i];
             }
         }
-        return Arrays.stream(dp).max().getAsInt();
+        return len;
     }
+    private static int BinarySearch(int[] dp, int si, int ei, int item) {
+        int idx = 0; 
+        while (si <= ei) {
+            int mid = (si+ei)/2;
+            if (dp[mid] >= item) {
+                idx = mid;
+                ei = mid - 1;
+            } else {
+                si = mid + 1;
+            }
+        }
+        return idx;
+    }
+
 }
