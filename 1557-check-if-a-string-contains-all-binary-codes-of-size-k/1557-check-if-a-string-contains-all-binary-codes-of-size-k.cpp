16553ff1c7cef04dcc78b3876ad975ad46af5683
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-09-29 19:30:32 +0530

    Time: 425 ms (60.8%), Space: 119.1 MB (59.8%) - LeetHub

---
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,22 @@
+/* APPROACH: Sliding Window */
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        int n = s.size();
+        if (n < k) return false;
+        unordered_set<string> seen;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            // Expand the Window Size
+            // Check the window size
+            if (right - left + 1 == k) {
+                seen.insert(s.substr(left, k));
+                // Shrink the window size
+                left++;
+            }
+        }
+        // Return the result after a check
+        return seen.size() == (1 << k);
+    }
+};
+// auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
