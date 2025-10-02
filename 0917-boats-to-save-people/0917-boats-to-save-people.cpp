d937b2f0fb67656305011aade6e9bcd47ffeca7f
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-02 19:55:29 +0530

    Time: 19 ms (46.69%), Space: 45.8 MB (69.08%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int i = 0, j = people.size() - 1;
+        int boats = 0;
+
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++; // lightest goes with heaviest
+            }
+            j--;     // heaviest always boards
+            boats++;
+        }
+        return boats;
+    }
+};
