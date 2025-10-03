709ea4351cc88b1e84c1158c601a09c2b53ca3ca
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-03 17:22:11 +0530

    Time: 3 ms (31.36%), Space: 17 MB (5.98%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void helper(int index, vector<int> nums, vector<int> path) {
+        // BASE CASE
+        if (index == nums.size()) {
+            ans.push_back(path);
+            return;
+        }
+        // Choice-1: Pick
+        path.push_back(nums[index]);
+        helper(index + 1, nums, path);
+
+        // Choice-2: Skip
+        path.pop_back();
+        helper(index + 1, nums, path);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> path;
+        helper(0, nums, path);
+        return ans;
+    }
+};
