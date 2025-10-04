7b3c5b38376f59a6dfe53a9fec4a7a734a6c3b53
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-04 20:10:28 +0530

    Time: 0 ms (100%), Space: 11 MB (29.48%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void helper(vector<int>& nums, vector<int>& curr, vector<int>& used) {
-        if (curr.size() == nums.size()) {
-            ans.push_back(curr);
+    // void helper(vector<int>& nums, vector<int>& curr, vector<int>& used) {
+    //     if (curr.size() == nums.size()) {
+    //         ans.push_back(curr);
+    //         return;
+    //     }
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         if (used[i]) continue;   // If in use then skip
+    //         used[i] = 1;             // Mark
+    //         curr.push_back(nums[i]); // Add it to current
+    //         helper(nums, curr, used);// Recursive Call
+    //         curr.pop_back();         // Unmark it - Backtrack
+    //         used[i] = 0;
+    //     }
+    // }
+
+    void helper(int index, vector<int>& nums) {
+        if (index == nums.size()) {
+            ans.push_back(nums);
             return;
         }
-        for (int i = 0; i < nums.size(); i++) {
-            if (used[i]) continue;   // If in use then skip
-            used[i] = 1;             // Mark
-            curr.push_back(nums[i]); // Add it to current
-            helper(nums, curr, used);// Recursive Call
-            curr.pop_back();         // Unmark it - Backtrack
-            used[i] = 0;
+        for (int i = index; i < nums.size(); i++) {
+            swap(nums[index], nums[i]);
+            helper(index + 1, nums);
+            swap(nums[index], nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> used(nums.size(), 0);
-        vector<int> curr;
-        helper(nums, curr, used);
+        // vector<int> used(nums.size(), 0);
+        // vector<int> curr;
+        // helper(nums, curr, used);
+        // return ans;
+        helper(0, nums);
         return ans;
     }
 };
