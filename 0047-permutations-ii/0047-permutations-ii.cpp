2c98eb0a7e0b48a4cd73c999e56fa8192c96c001
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-04 20:55:17 +0530

    Time: 11 ms (30.26%), Space: 15.9 MB (11.94%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,41 +1,57 @@
 /* METHOD-1: Used Array Based */
+// class Solution {
+// public:
+//     vector<vector<int>> ans;
+//     void helper(vector<int>& nums, vector<int>& curr, vector<int>& used) {
+//         if (curr.size() == nums.size()) {
+//             ans.push_back(curr);
+//             return;
+//         }
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (used[i]) continue;
+
+//             if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+
+//             used[i] = 1;
+//             curr.push_back(nums[i]);
+//             helper(nums, curr, used);
+//             curr.pop_back();
+//             used[i] = 0;
+//         }
+//     }
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         vector<int> curr;
+//         vector<int> used(nums.size(), 0);
+//         helper(nums, curr, used);
+//         return ans;
+//     }
+// };
+
+/* METHOD-2: Swap Based */
 class Solution {
 public:
     vector<vector<int>> ans;
-    void helper(vector<int>& nums, vector<int>& curr, vector<int>& used) {
-        if (curr.size() == nums.size()) {
-            ans.push_back(curr);
+    void helper(int index, vector<int>& nums) {
+        if (index == nums.size()) {
+            ans.push_back(nums);
             return;
         }
-        for (int i = 0; i < nums.size(); i++) {
-            if (used[i]) continue;
 
-            if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+        unordered_set<int> seen;
 
-            used[i] = 1;
-            curr.push_back(nums[i]);
-            helper(nums, curr, used);
-            curr.pop_back();
-            used[i] = 0;
+        for (int i = index; i < nums.size(); i++) {
+            if (seen.count(nums[i])) continue;
+            seen.insert(nums[i]);
+
+            swap(nums[index], nums[i]);
+            helper(index + 1, nums);
+            swap(nums[index], nums[i]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<int> curr;
-        vector<int> used(nums.size(), 0);
-        helper(nums, curr, used);
+        helper(0, nums);
         return ans;
     }
 };
-
-/* METHOD-2: Swap Based */
-// class Solution {
-// public:
-//     vector<vector<int>> ans;
-//     void helper() {
-
-//     }
-//     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        
-//     }
-// };
