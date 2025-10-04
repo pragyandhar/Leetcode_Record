716a4087ec1aad555d7ffb88b2bc38820a3e4ee3
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-04 18:30:37 +0530

    Time: 3 ms (71.44%), Space: 43.6 MB (14.44%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,24 +1,23 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(candidates);
-        findCombinations(0, candidates, target, ans, new ArrayList<>());
-        return ans;
-    }
-
-    private void findCombinations(int indx, int[] a, int t, List<List<Integer>> ans, List<Integer> ds) {
-        if (t == 0) {
-            ans.add(new ArrayList<>(ds));
+    List<List<Integer>> ans = new ArrayList<>();
+    public void helper(int index, int target, int[] candidates, List<Integer> curr) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(curr));
             return;
         }
-
-        for (int i = indx; i < a.length; i++) {
-            if (i > indx && a[i] == a[i-1]) continue;
-            if (a[i] > t) break;
-
-            ds.add(a[i]);
-            findCombinations(i + 1, a, t - a[i], ans, ds);
-            ds.remove(ds.size() - 1);
+        if (index == candidates.length) return;
+        for (int i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i] == candidates[i-1]) continue;
+            if (target >= candidates[i]) {
+                curr.add(candidates[i]);
+                helper(i+1, target - candidates[i], candidates, curr);
+                curr.remove(curr.size() - 1);
+            }
         }
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        helper(0, target, candidates, new ArrayList<>());
+        return ans;
     }
 }
