0e2b45f8d2cd8da416d78442af0cf210de1d80f6
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-04 21:13:32 +0530

    Time: 2 ms (71.95%), Space: 45.7 MB (8.35%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,40 +1,62 @@
 /* METHOD-1: Used Array */
-class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    public void helper(int[] nums, boolean[] used, List<Integer> curr) {
-        if (nums.length == curr.size()) {
-            ans.add(new ArrayList<>(curr));
-            return;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i]) continue;
-            
-            if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
-
-            used[i] = true;
-            curr.add(nums[i]);
-            helper(nums, used, curr);
-            curr.remove(curr.size() - 1);
-            used[i] = false;
-        } 
-    }
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        Arrays.sort(nums);
-        boolean[] used = new boolean[nums.length];
-        Arrays.fill(used, false);
-        helper(nums, used, new ArrayList<>());
-        return ans;
-    }
-}
-/* METHOD-2: Swap Based */
 // class Solution {
-//     public void Swap(int[] nums, int i, int j) {
-//         int temp = nums[i];
-//         nums[i] = nums[j];
-//         nums[j] = temp;
-//     }
+//     List<List<Integer>> ans = new ArrayList<>();
+//     public void helper(int[] nums, boolean[] used, List<Integer> curr) {
+//         if (nums.length == curr.size()) {
+//             ans.add(new ArrayList<>(curr));
+//             return;
+//         }
+//         for (int i = 0; i < nums.length; i++) {
+//             if (used[i]) continue;
+            
+//             if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
 
+//             used[i] = true;
+//             curr.add(nums[i]);
+//             helper(nums, used, curr);
+//             curr.remove(curr.size() - 1);
+//             used[i] = false;
+//         } 
+//     }
 //     public List<List<Integer>> permuteUnique(int[] nums) {
-        
+//         Arrays.sort(nums);
+//         boolean[] used = new boolean[nums.length];
+//         Arrays.fill(used, false);
+//         helper(nums, used, new ArrayList<>());
+//         return ans;
 //     }
 // }
+/* METHOD-2: Swap Based */
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public void Swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void helper(int index, int[] nums) {
+        if (index == nums.length) {
+            List<Integer> curr = new ArrayList<>();
+            for (int n : nums) curr.add(n);
+            ans.add(curr);
+            return;
+        }
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int i = index; i < nums.length; i++) {
+            if (seen.contains(nums[i])) continue;
+            seen.add(nums[i]);
+
+            Swap(nums, index, i);
+            helper(index + 1, nums);
+            Swap(nums, index, i);
+        }
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        helper(0, nums);
+        return ans;    
+    }
+}
