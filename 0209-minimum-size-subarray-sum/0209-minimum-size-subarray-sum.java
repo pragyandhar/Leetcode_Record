0519690bf9071a629a1f65580ef9ff3132360832
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-06 10:32:45 +0530

    Time: 10 ms (4.12%), Space: 57.2 MB (91.79%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,42 +1,3 @@
-/* BRUTE FORCE APPROACH: Brute Force - O(n2) */
-// class Solution {
-//     public int minSubArrayLen(int target, int[] nums) {
-//         int n = nums.length;
-//         int minLen = Integer.MAX_VALUE;
-//         for (int i = 0; i < n; i++) {
-//             int sum = 0;
-//             for (int j = i; j < n; j++) {
-//                 sum += nums[j];
-//                 if (sum >= target) {         
-//                     minLen = Math.min(minLen, j - i + 1);
-//                     break;                  
-//                 }
-//             }
-//         }
-//         return (minLen == Integer.MAX_VALUE) ? 0 : minLen;
-//     }
-// }
-
-/* OPTIMAL APPROACH: Sliding Window - O(n) */
-// class Solution {
-//     public int minSubArrayLen(int target, int[] nums) {
-//         int minLen = Integer.MAX_VALUE;
-//         int sum = 0;
-//         int left = 0;
-
-//         for (int right = 0; right < nums.length; right++) {
-//             sum += nums[right];
-            
-//             while (sum >= target) {
-//                 minLen = Math.min(minLen, right - left + 1);
-//                 sum -= nums[left];
-//                 left++;
-//             }
-//         }
-//         return minLen == Integer.MAX_VALUE ? 0 : minLen;
-//     }
-// }
-
 /* BETTER APPROACH: Binary Search over Prefix Sum - O(nlogn) + O(n) */
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
@@ -79,3 +40,44 @@ class Solution {
         return ans;
     }
 }
+
+
+/* BRUTE FORCE APPROACH: Brute Force - O(n2) */
+// class Solution {
+//     public int minSubArrayLen(int target, int[] nums) {
+//         int n = nums.length;
+//         int minLen = Integer.MAX_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             int sum = 0;
+//             for (int j = i; j < n; j++) {
+//                 sum += nums[j];
+//                 if (sum >= target) {         
+//                     minLen = Math.min(minLen, j - i + 1);
+//                     break;                  
+//                 }
+//             }
+//         }
+//         return (minLen == Integer.MAX_VALUE) ? 0 : minLen;
+//     }
+// }
+
+/* OPTIMAL APPROACH: Sliding Window - O(n) */
+// class Solution {
+//     public int minSubArrayLen(int target, int[] nums) {
+//         int minLen = Integer.MAX_VALUE;
+//         int sum = 0;
+//         int left = 0;
+
+//         for (int right = 0; right < nums.length; right++) {
+//             sum += nums[right];
+            
+//             while (sum >= target) {
+//                 minLen = Math.min(minLen, right - left + 1);
+//                 sum -= nums[left];
+//                 left++;
+//             }
+//         }
+//         return minLen == Integer.MAX_VALUE ? 0 : minLen;
+//     }
+// }
+
