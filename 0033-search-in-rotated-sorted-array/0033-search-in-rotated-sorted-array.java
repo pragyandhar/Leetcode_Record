ba086cfcf7377386e3d6d63126af11cbcb652b4e
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-09 00:27:17 +0530

    Time: 0 ms (100%), Space: 42 MB (69.64%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,32 +1,35 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    public int findPivot(int[] n) {
         int low = 0;
-        int high = nums.length-1;
+        int high = n.length-1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (n[mid] > n[high]) low = mid + 1;
+            else high = mid;
+        }
+
+        return low;
+    } 
+
+    public int BS(int[] n, int t, int low, int high) {
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            // == Condition
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            //LEFT half is rotated
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-
-            //RIGHT half is rotated
-            else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
+            if (n[mid] == t) return mid;
+            else if (n[mid] < t) low = mid + 1;
+            else high = mid-1;
         }
         return -1;
+    }
+
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int pivot = findPivot(nums);
+
+        int left = BS(nums, target, 0, pivot-1);
+        if (left != -1) return left;
+
+        return BS(nums, target, pivot, n-1);
     }
 }
