07998cb3b69e91e8e1de2c5f5ec38e968a875c0c
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-08 22:51:52 +0530

    Time: 0 ms (100%), Space: 31.2 MB (98.32%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     // Helper
@@ -14,5 +15,27 @@ public:
     int search(vector<int>& a, int t) {
         int st = 0, end = a.size()-1;
         return bs(a, t, st, end);
+    }
+};
+*/
+class Solution {
+public:
+    int search(vector<int>& a, int t) {
+        int n = a.size();
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (a[mid] == t) {
+                return mid;
+            } else 
+            if (a[mid] < t) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 };
