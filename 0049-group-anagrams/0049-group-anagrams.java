7a6d8868fa9a4305a2c997b02a6d725b359f37c9
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-13 22:35:19 +0530

    Time: 7 ms (63.16%), Space: 47.8 MB (70.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,34 +1,12 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List> map = new HashMap<>();
-        for(String s:strs){
-            int [] count = new int[26];
-            for(char c : s.toCharArray()){
-                count[c-'a'] ++; 
-            }
-            StringBuilder sb  = new StringBuilder();
-            for(int i:count){
-                sb.append("#");
-                sb.append(i);
-            }
-            String key = sb.toString();
-            if(!map.containsKey(key)){
-                map.put(key,new ArrayList<String>());
-            }
-            map.get(key).add(s);
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = new String(ca);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
-        return new ArrayList(map.values());
-    }
-    public static String GetKey(String s) {
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            freq[ch-'a']++;
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < freq.length; i++) {
-            sb.append(freq[i] + " ");
-        }
-        return sb.toString();
+        return new ArrayList<>(map.values());
     }
 }
