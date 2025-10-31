04c6b9eaf1e58159fb079f25955d3802bc28100a
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-10-31 15:25:41 +0530

    Time: 112 ms (31.43%), Space: 69.3 MB (41.67%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -7,21 +7,34 @@
 // @lc code=start
 class Solution {
 public:
+    // int maxOperations(vector<int>& nums, int k) {
+    //     int i = 0;
+    //     int j = size(nums)-1;
+    //     int operations = 0;
+    //     sort(nums.begin(), nums.end());
+    //     while (i < j) {
+    //         int sum = nums[i] + nums[j];
+    //         if (sum == k) {
+    //             i++;
+    //             j--;
+    //             operations++;
+    //         } else if (sum > k) {
+    //             j--;
+    //         } else {
+    //             i++;
+    //         }
+    //     }
+    //     return operations;
+    // }
     int maxOperations(vector<int>& nums, int k) {
-        int i = 0;
-        int j = size(nums)-1;
+        unordered_map<int, int> mp;
         int operations = 0;
-        sort(nums.begin(), nums.end());
-        while (i < j) {
-            int sum = nums[i] + nums[j];
-            if (sum == k) {
-                i++;
-                j--;
+        for (int x : nums) {
+            if (mp.find(k-x) != mp.end() && mp[k-x] > 0) {
+                mp[k-x]--;
                 operations++;
-            } else if (sum > k) {
-                j--;
             } else {
-                i++;
+                mp[x]++;
             }
         }
         return operations;
