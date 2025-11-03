9a302a5d425690fb8a064998ff7aea06472a4c18
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-11-03 20:03:03 +0530

    Time: 0 ms (100%), Space: 8.9 MB (53.63%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,17 +1,37 @@
+// class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+//         stack<char> st;
+//         string res = "";
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] == '(') {
+//                 if (!st.empty()) res += s[i]; 
+//                 st.push(s[i]);
+//             } else {
+//                 st.pop();
+//                 if (!st.empty()) res += s[i];
+//             } 
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<char> st;
-        string res = "";
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(') {
-                if (!st.empty()) res += s[i]; 
-                st.push(s[i]);
-            } else {
-                st.pop();
-                if (!st.empty()) res += s[i];
-            } 
+        string result;
+        int depth = 0;  // track current level of nesting
+        for (char c : s) {
+            if (c == '(') {
+                // if depth > 0, it means it's not the outermost '('
+                if (depth > 0) result += c;
+                depth++;
+            } else { // c == ')'
+                depth--;
+                // if depth > 0 after decrement, it's not the outermost ')'
+                if (depth > 0) result += c;
+            }
         }
-        return res;
+        return result;
     }
 };
