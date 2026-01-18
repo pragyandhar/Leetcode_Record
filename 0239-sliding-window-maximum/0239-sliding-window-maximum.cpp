b852edd9f777e757082f3f7f5485bdcf27339b5f
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2026-01-19 00:39:18 +0530

    Time: 16 ms (85.04%), Space: 139.1 MB (87.76%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int>dq;
-        vector<int>ans;
-        int i = 0;
-        int j = 0;
-        
-        while(j < nums.size()){
-            //calculation here
-            while(dq.size() > 0 && dq.back() < nums[j]){
-                dq.pop_back();
-            }
-            //insert dq
-            dq.push_back(nums[j]);
-            //window size < k Then j move 
-            if(j - i + 1 < k){
-                j++;
-            }else if(j - i + 1 == k){
-                //ans update
-                if(!dq.empty()){
-                    ans.push_back(dq.front());
-                }
-                //slide the window 
-                if(dq.front() == nums[i]){
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        int n = arr.size();
+        deque<int> dq;
+        vector<int> ans;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            // EXPAND
+            while (!dq.empty() && dq.back() < arr[right]) dq.pop_back();
+            dq.push_back(arr[right]);
+            // CHECK WINDOW SIZE
+            if (right - left + 1 == k) {
+                ans.push_back(dq.front());
+                // SHRINK
+                if (dq.front() == arr[left]) {
                     dq.pop_front();
                 }
-                i++;
-                j++;
+                left++;
             }
         }
         return ans;
