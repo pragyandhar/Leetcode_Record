a20eda7a00dc0ea459ffd1a5072add1f241c68b2
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2026-02-03 11:50:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (31.03%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+        int ans = 0;
+        while (target > 1 && maxDoubles > 0) {
+            if (target % 2 != 0) {
+                target--;
+                ans++;
+            } else {
+                target /= 2;
+                ans++;
+                maxDoubles--;
+            }
+        }
+        ans += target-1;
+        return ans;
+    }
+};
