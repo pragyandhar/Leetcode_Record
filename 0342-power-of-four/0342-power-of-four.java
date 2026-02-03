9e22094bfbfe2eced572a12f3754e9bda636b2ab
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2026-02-03 13:44:47 +0530

    Time: 1 ms (88.85%), Space: 42.8 MB (26.98%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
-    }
+       
+         
+        while(n>=4){
+            if(n%4!=0) return false;
+            n/=4;
+        }
+        return n==1;
+    } 
+    
 }
